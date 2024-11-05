2b83c74b39634cdb459a9cfdeed6fc679daedccc
nicholaslord5 <nicholas@nicholaslord.com>
2024-11-05 01:27:33 -0900

    first commit

---
+++ mood_responses.py
@@ -0,0 +1,18 @@
+ # mood_responses.py
+def mood_response(mood):
+        if mood == "happy":
+            return "That's awesome! Keept the good times rolling!"
+        elif mood == "sad" or mood == "down":
+            return "It's ok, we all feel that way sometimes. Wanna talk about it?"
+        elif mood == "angry" or mood == "mad":
+            return "What's got you upset? Would you like to do some breathing exercises?"
+        elif mood == "nervous" or mood == "anxious":
+            return "If you'd like, sometimes it helps to occupy your time with other activities, or go for a walk outside."
+        elif mood == "panic" or mood == "panicked":
+            return "It's going to be all right. Try breathing as slow and deep as you can."
+        elif mood == "excited":
+            return "Fantastic! I'm excited for you, too!"
+        elif mood == "tired" or mood == "sleepy":
+            return "Sounds like it's time for bed or a nap. Breaks are good and necessary. Sit back and relax a bit."
+        else:
+            return "That's ok. Would you like to talk about whatever is on your mind?"
